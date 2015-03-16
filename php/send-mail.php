be93ea88289a31ee1db60044ec7957e6c5aa26cd
Axor <gadiaz3@gmail.com>
2015-03-16 17:54:41 -0600

    Functionall PHP contact form
    Missing the success and error results.

--- php/send-mail.php
+++ php/send-mail.php
@@ -5,21 +5,25 @@
     $lastName = $_POST['lastName'];
     $mailFrom = $_POST['emailAddress'];
     $message = $_POST['messageContent'];
-    $name = $fname . $lastName;
+    $name = $fName . ' ' . $lastName;
  
     $mail_to = 'gadiaz3@gmail.com'; // specify your email here
 
     // Construct email subject
-    $subject = 'gusdiaz-me : Message from: ' . $name;
+    $subject = 'GUSDIAZ.ME Message from: ' . $name;
  
     // Construct email body
-    $body_message = 'From: ' . $name . "\r\n";
-    $body_message .= 'E-mail: ' . $mailFrom . "\r\n";
-    $body_message .= 'Message: ' . $message;
+    $body_message = 'Hello user, ' . $name . ' has send you a message from your contact form in gusdiaz.me.' . "\r\n" . "\r\n";
+    $body_message .= 'From: ' . $name . "\r\n";
+    $body_message .= 'E-mail: ' . $mailFrom . "\r\n" . "\r\n";
+    $body_message .= 'Message: ' . "\r\n" . "\r\n" . $message;
  
     // Construct email headers
     $headers = 'From: ' . $mailFrom . "\r\n";
     $headers .= 'Reply-To: ' . $mailFrom . "\r\n";
+
+    $mail_sent = mail($mail_to, $subject, $body_message, $headers);
  
-    echo $name;
+    echo "Message Sent!";
+    
 ?>
